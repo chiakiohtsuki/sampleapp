1c0717e2c7b724efcedfcf669942606f2e273c9b
ootsuki chiaki <chiakinyankoko@gmail.com>
2020-03-20 14:40:09 +0900

    recreat

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
-Rails.application.routes.draw do
-    root 'application#hello'
-  end
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
